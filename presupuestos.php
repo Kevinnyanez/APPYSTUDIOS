b4f0c782dc0a4f05430595ad1df85483c5b7fd90
Kevinnyanez <kevinnyanez01@gmail.com>
2025-05-29 23:16:03 -0300

    w

--- presupuestos.php
+++ presupuestos.php
@@ -24,7 +24,7 @@ $stock_items = [];
 while ($row = $stock_result->fetch_assoc()) {
     $stock_items[] = $row;
 }
-require_once 'dompdf/autoload.inc.php';  // ajusta la ruta si hace falta
+  // ajusta la ruta si hace falta
 
 if (isset($_GET['descargar_pdf'])) {
     require_once 'dompdf/autoload.inc.php';
