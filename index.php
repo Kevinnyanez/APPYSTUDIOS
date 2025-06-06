76c84c94e77f786ff9a3305b14b4e478a059fa4e
Kevinnyanez <kevinnyanez01@gmail.com>
2025-06-06 17:24:01 -0300

    s

--- index.php
+++ index.php
@@ -173,10 +173,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
  .noticias {
   position: absolute;
   top: 50%;
-  right: 40px; /* Más a la derecha */
+  right: 55px; /* Más a la derecha */
   transform: translateY(-50%);
   background: #f9f9f9;
-  padding: 20px;
+  padding: 30px;
   border-radius: 10px;
   width: 320px;
   box-shadow: 0 0 10px rgba(0,0,0,0.1);
